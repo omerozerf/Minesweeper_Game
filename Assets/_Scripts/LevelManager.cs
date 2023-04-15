67d4006b4439188262cee67c3c39e4f1c73bb9bb
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-15 15:32:54 +0300

    menu

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -1,18 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelManager : MonoBehaviour
+namespace _Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LevelManager : MonoBehaviour
     {
+        public static LevelManager Instance { get; private set; }
         
-    }
+        
+        [SerializeField] private Transform x3GameObject;
+        [SerializeField] private Transform x5GameObject;
+        [SerializeField] private Transform x8GameObject;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField] private Button x3Button;
+        [SerializeField] private Button x5Button;
+        [SerializeField] private Button x8Button;
         
+        
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Debug.LogError("There is a more than Level Manager!" + transform + "-" + Instance);
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+
+        private void Start()
+        {
+            x3Button.onClick.AddListener((() => 
+                ShowX3()));
+            
+            x5Button.onClick.AddListener((() => 
+                ShowX5()));
+            
+            x8Button.onClick.AddListener((() => 
+                ShowX8()));
+        }
+
+
+        public void ShowX3()
+        {
+            x3GameObject.gameObject.SetActive(true);
+            HideX5();
+            HideX8();
+        }
+    
+    
+        public void ShowX5()
+        {
+            x5GameObject.gameObject.SetActive(true);
+            HideX3();
+            HideX8();
+        }
+    
+    
+        public void ShowX8()
+        {
+            x8GameObject.gameObject.SetActive(true);
+            HideX3();
+            HideX5();
+        }
+    
+    
+        public void HideX3()
+        {
+            x3GameObject.gameObject.SetActive(false);
+        }
+    
+    
+        public void HideX5()
+        {
+            x5GameObject.gameObject.SetActive(false);
+        }
+    
+    
+        public void HideX8()
+        {
+            x8GameObject.gameObject.SetActive(false);
+        }
     }
 }
--- Assets/_Scripts/StartButton.cs
+++ Assets/_Scripts/StartButton.cs
@@ -7,12 +7,22 @@ namespace _Scripts
     public class StartButton : MonoBehaviour
     {
         [SerializeField] private Button startButton;
+        [SerializeField] private Transform menuGameObject;
+        [SerializeField] private Transform inGameObject;
 
 
         private void Start()
         {
             startButton.onClick.AddListener(() =>
                 GameController.Instance.StartGame()
+                );
+            
+            startButton.onClick.AddListener(() =>
+                menuGameObject.gameObject.SetActive(false)
+                );
+            
+            startButton.onClick.AddListener(() =>
+                inGameObject.gameObject.SetActive(true)
                 );
         }
     }
