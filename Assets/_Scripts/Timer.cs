e3888657523eff89c0fbb22874af91781e16f720
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-16 23:03:06 +0300

    best time fixlemeyi unutma - ilk tıkladığın yerde bomba cıkmasın

--- Assets/_Scripts/Timer.cs
+++ Assets/_Scripts/Timer.cs
@@ -1,23 +1,43 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Scripts
 {
     public class Timer : MonoBehaviour
     {
+        [FormerlySerializedAs("bestScore")] public int bestTime;
+
+
         [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private TextMeshProUGUI bestTimeText;
+        
 
 
-        private float time = 0f;
-        
-        
+        private float currentTime;
+
+
+        private void Start()
+        {
+            bestTime = PlayerPrefs.GetInt("BestScore", 0);
+            bestTimeText.text = "Best Time:" + bestTime;
+        }
+
         private void Update()
         {
             if (GameController.Instance.isFinish ||
                 GameController.Instance.isGameWin) return;
             
-            time += Time.deltaTime;
-            text.text = time.ToString("0000");
+            currentTime += Time.deltaTime;
+            text.text = currentTime.ToString("0000");
+
+            if (currentTime < bestTime && GameController.Instance.isGameWin)
+            {
+                bestTime = (int) currentTime;
+                PlayerPrefs.SetInt("BestScore", bestTime);
+            }
+
         }
     }
 }
