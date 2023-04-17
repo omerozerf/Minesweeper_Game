099ab433b80c8b01b5bc86b735b1a579cc7e2204
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-04-17 03:29:07 +0300

    Update GameController.cs

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using MyGrid.Code;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 namespace _Scripts
